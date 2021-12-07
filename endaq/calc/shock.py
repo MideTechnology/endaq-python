68859505af72e7716744311ef46ffed2c2b4cc10
Becker Awqatty <bawqatty@mide.com>
2021-12-07 12:38:54 -0500

    rearranged code blocks

--- endaq/calc/shock.py
+++ endaq/calc/shock.py
@@ -232,54 +232,6 @@ def shock_spectrum(
     )
 
 
-def enveloping_half_sine(
-    pvss: pd.DataFrame,
-    damp: float = 0.0,
-) -> HalfSineWavePulse:
-    """
-    Characterize a half-sine pulse whose PVSS envelopes the input.
-
-    :param pvss: the PVSS to envelope
-    :param damp: the damping factor used to generate the input PVSS
-    :return: a tuple of amplitudes and periods, each pair of which describes a
-        half-sine pulse
-
-    .. seealso::
-
-        `Pseudo Velocity Shock Spectrum Rules For Analysis Of Mechanical Shock, Howard A. Gaberson <https://info.endaq.com/hubfs/pvsrs_rules.pdf>`_
-    """
-
-    def amp_factor(damp):
-        """
-        Calculate the PVSS amplitude attenuation on a half-sine pulse from the
-        damping coefficient.
-
-        The PVSS of a half-sine pulse differs based on the damping coefficient
-        used. While the high-frequency rolloff is relatively consistent, the
-        flat low-frequency amplitude is attenuated at higher damping values.
-        This function calculates this attenuation for a given damping
-        coefficient.
-        """
-        # This calculates the PVSS value as ω->0. However, since it necessarily
-        # computes the maximum of a function *over time*, and ω is only found
-        # therein in the multiplicative factor (ωt), it is mathematically
-        # equivalent to compute this maximum for any arbitrary ω>0. Thus we
-        # choose ω=1 for convenience, w/o loss of generality.
-        a = np.exp(1j * np.arccos(-damp))  # = -damp + 1j * np.sqrt(1 - damp**2)
-        # From WolframAlpha: https://www.wolframalpha.com/input/?i=D%5BPower%5Be%2C%5C%2840%29-d+*t%5C%2841%29%5D+sin%5C%2840%29Sqrt%5B1-Power%5Bd%2C2%5D%5D*t%5C%2841%29%2Ct%5D+%3D+0&assumption=%22ListOrTimes%22+-%3E+%22Times%22&assumption=%7B%22C%22%2C+%22e%22%7D+-%3E+%7B%22NamedConstant%22%7D&assumption=%7B%22C%22%2C+%22d%22%7D+-%3E+%7B%22Variable%22%7D&assumption=%22UnitClash%22+-%3E+%7B%22d%22%2C+%7B%22Days%22%7D%7D
-        t_max = (2 / np.imag(a)) * np.arctan2(np.imag(a), 1 - np.real(a))
-        PVSS_max = (1 / np.imag(a)) * np.imag(np.exp(a * t_max))
-        return PVSS_max
-
-    max_pvss = pvss.max()
-    max_f_pvss = pvss.mul(pvss.index, axis=0).max()
-
-    return HalfSineWavePulse(
-        amplitude=2 * np.pi * max_f_pvss,
-        duration=max_pvss / (4 * amp_factor(damp) * max_f_pvss),
-    )
-
-
 class HalfSineWavePulse(NamedTuple):
     """
     The output data type for ``enveloping_half_sine``.
@@ -359,3 +311,51 @@ class HalfSineWavePulse(NamedTuple):
 
     # def pseudo_velocity(self):
     #    pass
+
+
+def enveloping_half_sine(
+    pvss: pd.DataFrame,
+    damp: float = 0.0,
+) -> HalfSineWavePulse:
+    """
+    Characterize a half-sine pulse whose PVSS envelopes the input.
+
+    :param pvss: the PVSS to envelope
+    :param damp: the damping factor used to generate the input PVSS
+    :return: a tuple of amplitudes and periods, each pair of which describes a
+        half-sine pulse
+
+    .. seealso::
+
+        `Pseudo Velocity Shock Spectrum Rules For Analysis Of Mechanical Shock, Howard A. Gaberson <https://info.endaq.com/hubfs/pvsrs_rules.pdf>`_
+    """
+
+    def amp_factor(damp):
+        """
+        Calculate the PVSS amplitude attenuation on a half-sine pulse from the
+        damping coefficient.
+
+        The PVSS of a half-sine pulse differs based on the damping coefficient
+        used. While the high-frequency rolloff is relatively consistent, the
+        flat low-frequency amplitude is attenuated at higher damping values.
+        This function calculates this attenuation for a given damping
+        coefficient.
+        """
+        # This calculates the PVSS value as ω->0. However, since it necessarily
+        # computes the maximum of a function *over time*, and ω is only found
+        # therein in the multiplicative factor (ωt), it is mathematically
+        # equivalent to compute this maximum for any arbitrary ω>0. Thus we
+        # choose ω=1 for convenience, w/o loss of generality.
+        a = np.exp(1j * np.arccos(-damp))  # = -damp + 1j * np.sqrt(1 - damp**2)
+        # From WolframAlpha: https://www.wolframalpha.com/input/?i=D%5BPower%5Be%2C%5C%2840%29-d+*t%5C%2841%29%5D+sin%5C%2840%29Sqrt%5B1-Power%5Bd%2C2%5D%5D*t%5C%2841%29%2Ct%5D+%3D+0&assumption=%22ListOrTimes%22+-%3E+%22Times%22&assumption=%7B%22C%22%2C+%22e%22%7D+-%3E+%7B%22NamedConstant%22%7D&assumption=%7B%22C%22%2C+%22d%22%7D+-%3E+%7B%22Variable%22%7D&assumption=%22UnitClash%22+-%3E+%7B%22d%22%2C+%7B%22Days%22%7D%7D
+        t_max = (2 / np.imag(a)) * np.arctan2(np.imag(a), 1 - np.real(a))
+        PVSS_max = (1 / np.imag(a)) * np.imag(np.exp(a * t_max))
+        return PVSS_max
+
+    max_pvss = pvss.max()
+    max_f_pvss = pvss.mul(pvss.index, axis=0).max()
+
+    return HalfSineWavePulse(
+        amplitude=2 * np.pi * max_f_pvss,
+        duration=max_pvss / (4 * amp_factor(damp) * max_f_pvss),
+    )
