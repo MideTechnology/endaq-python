115ed2e058803c372d51dfebb2cdf2fc8185e6fd
Becker Awqatty <bawqatty@mide.com>
2021-12-08 11:10:28 -0500

    fixup: removed impossibly small highpass cutoffs

--- tests/calc/test_integrate.py
+++ tests/calc/test_integrate.py
@@ -61,7 +61,7 @@ def test_integrals():
         dtype=np.float64,
     ).map(lambda array: pd.DataFrame(array, index=0.2 * np.arange(20))),
     zero=hyp_st.sampled_from(["start", "mean", "median"]),
-    highpass_cutoff=hyp_st.floats(0, 1).map(lambda x: x or None),
+    highpass_cutoff=hyp_st.one_of(hyp_st.floats(0.25, 1), hyp_st.just(None)),
     filter_half_order=hyp_st.integers(1, 3),
     tukey_percent=hyp_st.floats(0, 0.2),
 )
