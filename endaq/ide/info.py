0b9a5687e0ad685618febe02538aa858e1b17483
Becker Awqatty <bawqatty@mide.com>
2021-12-09 15:59:39 -0500

    fixed docstring

--- endaq/ide/info.py
+++ endaq/ide/info.py
@@ -304,9 +304,10 @@ def to_pandas(
         :kwarg time_mode: how to temporally index samples; each mode uses either
             relative times (with respect to the start of the recording) or
             absolute times (i.e., date-times):
-            - "seconds" - a `pandas.Float64Index` of relative timestamps, in seconds
-            - "timedelta" - a `pandas.TimeDeltaIndex` of relative timestamps
-            - "datetime" - a `pandas.DateTimeIndex` of absolute timestamps
+
+            - `"seconds"` - a `pandas.Float64Index` of relative timestamps, in seconds
+            - `"timedelta"` - a `pandas.TimeDeltaIndex` of relative timestamps
+            - `"datetime"` - a `pandas.DateTimeIndex` of absolute timestamps
         :return: a `pandas.DataFrame` containing the channel's data
     """
     data = channel.getSession().arraySlice()
@@ -319,7 +320,7 @@ def to_pandas(
         t = t + np.datetime64(channel.dataset.lastUtcTime, "s")
     elif time_mode != "timedelta":
         raise ValueError(f'invalid time mode "{time_mode}"')
-    
+
     if hasattr(channel, "subchannels"):
         columns = [sch.name for sch in channel.subchannels]
     else:
