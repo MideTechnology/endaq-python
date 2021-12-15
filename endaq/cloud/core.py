75003d74e3ad6c75ffa40ee5492a179a283d2961
DESKTOP-ISI35E2\SamRagusa <samragusa@gmail.com>
2021-12-15 17:10:02 -0500

    Minor Refactors

--- endaq/cloud/core.py
+++ endaq/cloud/core.py
@@ -125,8 +125,11 @@ class EndaqCloud:
 
         return Dataset(f)
 
-    def download_all_ide_files(
-            self, output_directory: str = "", should_download_file_fn: Callable = lambda x: True, force_recompute_file_table: bool = False, file_limit: int = 100):
+    def download_all_ide_files(self,
+                               output_directory: str = "",
+                               should_download_file_fn: Optional[Callable] = None,
+                               force_reload_file_table: bool = False,
+                               file_limit: int = 100) -> np.ndarray:
         """
         Downloads all IDE files from the enDAQ Cloud (up to a specified file limit).
 
@@ -134,7 +137,7 @@ class EndaqCloud:
         :param should_download_file_fn: A function which accepts a row of the IDE file table and returns
          a boolean value which indicates if the IDE file should be downloaded or not.  If this function is
          not given, the default function will always return True.
-        :param force_recompute_file_table: If the file table to use as reference for what files exist in the cloud
+        :param force_reload_file_table: If the file table to use as reference for what files exist in the cloud
          should be recomputed even if it is already stored (as `self.file_table`)
         :param file_limit: The maximum number of files to download.  If the `force_recompute_file_table` parameter
          is True then this will also be used to limit the number of files put in the file table it creates.
@@ -148,15 +151,18 @@ class EndaqCloud:
             raise TypeError('the "output_directory" parameter must be given a string,'
                             f'but was given a value of type{type(output_directory)}')
 
-        if not isinstance(force_recompute_file_table, bool):
+        if not isinstance(force_reload_file_table, bool):
             raise TypeError('the "force_recompute_file_table" parameter must be given a value of type bool,'
-                            f'but was given a value of type{type(force_recompute_file_table)}')
+                            f'but was given a value of type{type(force_reload_file_table)}')
 
         if not isinstance(file_limit, (int, np.integer)):
             raise TypeError('the "file_limit" parameter must be given a value of type int or np.integer,'
                             f'but was given a value of type{type(file_limit)}')
 
-        if self.file_table is None or force_recompute_file_table:
+        if should_download_file_fn is None:
+            should_download_file_fn = lambda x: True
+
+        if self.file_table is None or force_reload_file_table:
             self.get_file_table(attributes=[], limit=file_limit)
 
         failures = []
