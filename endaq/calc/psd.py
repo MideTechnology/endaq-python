f80c01fef9343c8057220fb472091fdc8115565e
shanlyMIDE <35080650+shanlyMIDE@users.noreply.github.com>
2022-04-07 17:12:35 -0400

    rolling_psd

--- endaq/calc/psd.py
+++ endaq/calc/psd.py
@@ -335,3 +335,89 @@ def vc_curves(
     # The PSD must already scale by ∆f -> need only scale by √∆f?
     # TODO make `density` parameter, scale differently depending on mode
     return np.sqrt(accel_psd.index[1]) * df_vel_oct.apply(np.sqrt)
+
+
+def rolling_psd(
+        df: pd.DataFrame,
+        bin_width: float = 1.0,
+        octave_bins: float = None,
+        fstart: float = 1.0,
+        num_slices: int = 100,
+        indexes: np.array = None,
+        index_values: np.array = None,
+        slice_width: float = None,
+        add_resultant: bool = True,
+        disable_warnings: bool = True,
+        **kwargs,
+) -> pd.DataFrame:
+    """
+    Compute PSDs for defined slices of a time series data set using :py:func:`~endaq.calc.psd.welch()`
+    :param df: the input dataframe with an index defining the time in seconds or datetime
+    :param bin_width: the bin width or resolution in Hz for the PSD, defaults to 1,
+        this is ignored if `octave_bins` is defined
+    :param octave_bins: the number of frequency bins in each octave, defaults to `None`
+    :param fstart: the lowest frequency for an octave PSD, defaults to 1
+    :param num_slices: the number of slices to split the time series into, default is 100,
+        this is ignored if `indexes` is defined
+    :param indexes: the center index locations (not value) of each slice to compute the PSD
+    :param index_values: the index values of each peak event to quantify (slower but more intuitive than using `indexes`)
+    :param slice_width: the time in seconds to center about each slice index,
+        if none is provided it will calculate one based upon the number of slices
+    :param add_resultant: if `True` (default) the root sum of squares of each PSD column will
+        also be computed
+    :param disable_warnings: if `True` (default) it disables the warnings on the PSD length
+    :param kwargs: Other parameters to pass directly to :py:func:`psd.welch`
+    :return: a dataframe containing all the PSDs, stacked on each other
+
+    See example use cases and syntax at :py:func:`~endaq.plot.plots.spectrum_over_time()`
+    which visualizes the output of this function in Heatmaps, Waterfall plots, 
+    Surface plots, and Animations
+
+    """
+    if disable_warnings:
+        warnings.filterwarnings('ignore', '.*greater than.*', )
+        warnings.filterwarnings('ignore', '.*empty frequency*', )
+
+    length = len(df)
+
+    #Define center index locations of each slice if not provided
+    if indexes is None:
+        if index_values is not None:
+            indexes = np.zeros(len(index_values),int)
+            for i in range(len(indexes)):
+                indexes[i] = int((np.abs(df.index - index_values[i])).argmin())
+        else:
+            indexes = np.linspace(0, length, num_slices, endpoint = False, dtype=int)
+            indexes = indexes + int(indexes[1]/2)
+
+    #Calculate slice step size
+    spacing = utils.sample_spacing(df)
+    if slice_width is None:
+        slice_width = spacing * length / len(indexes)
+    num = int(slice_width / spacing / 2)
+
+    #Define bin_width if octave spacing
+    if octave_bins is not None:
+        bin_width = 1 / slice_width
+
+    #Loop through and compute PSD
+    psd = pd.DataFrame()
+    for i in indexes:
+        window_start = max(0, i - num)
+        window_end = min(length, i + num)
+        slice_psd = welch(
+            df.iloc[window_start:window_end],
+            bin_width = bin_width,
+            **kwargs
+        )
+        if octave_bins is not None:
+            slice_psd = to_octave(slice_psd, octave_bins=octave_bins, fstart = fstart)
+
+        if add_resultant:
+            slice_psd['Resultant'] = slice_psd.sum(axis=1)
+
+        slice_psd = slice_psd.reset_index().melt(id_vars=slice_psd.index.name)
+        slice_psd['timestamp'] = df.index[i]
+        psd = pd.concat([psd,slice_psd])
+
+    return psd
