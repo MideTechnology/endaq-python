dd0445bd33c6bb6bfbdcd0e2a7172d7a54a3f38b
MIDE\shanly <shanly@mide.com>
2022-06-02 12:02:41 -0400

    only specifying hypothesis

--- setup.py
+++ setup.py
@@ -23,24 +23,24 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 INSTALL_REQUIRES = [
     "backports.cached-property; python_version<'3.8'",
-    "ebmlite == 3.3.0",
-    "idelib == 3.2.4",
-    "jinja2 == 3.1.1",
-    "numpy == 1.22.3",
-    "pandas == 1.4.2",
-    "plotly == 5.7.0",
-    "python-dotenv == 0.20.0",
-    "requests == 2.27.1",
-    "scipy == 1.8.0",
+    "ebmlite>=3.2.0",
+    "idelib>=3.2.3",
+    "jinja2",
+    "numpy>=1.19.5",
+    "pandas>=1.3",
+    "plotly>=5.3.1",
+    "python-dotenv>=0.18.0",
+    "requests>=2.25.1",
+    "scipy>=1.7.1",
 ]
 
 TEST_REQUIRES = [
     "hypothesis == 6.41.0",
-    "pytest == 7.1.2",
-    "pytest-cov == 3.0.0",
-    "sympy == 1.10.1",
+    "pytest",
+    "pytest-cov",
     "pytest-xdist[psutil]",
-    ]
+    "sympy",
+]
 
 DOCS_REQUIRES = [
     "sphinx",
