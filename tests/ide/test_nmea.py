67732c028c16b7609e9e17a03495504098297828
David Stokes <dstokes@mide.com>
2022-09-16 17:16:58 -0400

    requirements update, minor test fix

--- tests/ide/test_nmea.py
+++ tests/ide/test_nmea.py
@@ -17,7 +17,7 @@ def junk():
 @pytest.fixture()
 def ide_doc():
     """The IDE document to use for testing the parser"""
-    return get_doc(os.path.abspath("../../tests/ide/nmea.IDE"))
+    return get_doc("/nmea.IDE")
 
 
 @pytest.fixture()
