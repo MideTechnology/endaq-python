ffad7aacd447d3a48560964345495d0a08470012
Jayden Personnat <89941534+jaydenpersonnat@users.noreply.github.com>
2024-06-14 10:53:20 -0400

    check for python<3.9
    Co-authored-by: David Randall Stokes <dstokes@mide.com>

--- endaq/batch/core.py
+++ endaq/batch/core.py
@@ -213,7 +213,7 @@ def _make_peak_windows(ch_data_cache: analyzer.CalcCache, margin_len):
     # Format results
     # Use new implementation of future_stack if Python version >= 3.9
     levels = ["axis", "peak time", "peak offset"]
-    if minor_version < 9:
+    if sys.version_info< (3, 9):
         return aligned_peak_data.stack().stack().reorder_levels(levels)
 
     return aligned_peak_data.stack(future_stack=True).stack().reorder_levels(levels)
