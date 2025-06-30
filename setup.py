97ed61d2f6f934dc49ab7097cd12e4f1901c7711
Elin O'Neill <eoneill@hutchinsoninc.com>
2025-06-30 11:31:52 -0400

    Revision to NumPy limit 2.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ INSTALL_REQUIRES = [
     "ebmlite>=3.2.0",
     "idelib>=3.2.8",
     "jinja2",
-    "numpy>1.19.5,<=2.3.0",
+    "numpy>1.19.5,<2.3.0",
     "pandas>=1.3",
     "plotly>=5.3.1",
     "pynmeagps",
