1f3afa5234a2d32608a8350cf9b7a0fabea1350c
Jeffrey S <jeffrey.smithwick@gmail.com>
2026-04-03 14:57:41 -0400

    added accelerometer bound tests for PE and PR sensors

--- tests/ide/test_util.py
+++ tests/ide/test_util.py
@@ -5,13 +5,22 @@ from endaq.ide.util import get_accelerometer_info, get_accelerometer_bounds
 import os
 
 IDE_FILENAME = os.path.join(os.path.dirname(__file__), "test.ide")
-
-
+PR_IDE_FILENAME = os.path.join(os.path.dirname(__file__), "../batch/DAQ09576_000001.IDE")
+PE_IDE_FILENAME = os.path.join(os.path.dirname(__file__), "../batch/test1.IDE'")
 @pytest.fixture
 def test_IDE():
     with importFile(IDE_FILENAME) as ds:
         yield ds
 
+@pytest.fixture
+def test_PR_IDE():
+    with importFile(IDE_FILENAME) as ds:
+        yield ds
+
+@pytest.fixture
+def test_PE_IDE():
+    with importFile(IDE_FILENAME) as ds:
+        yield ds
 
 def test_get_accelerometer_info(test_IDE):
     """
@@ -41,18 +50,19 @@ def test_incorrect_sensor_get_accelerometer_info(test_IDE):
             with pytest.raises(ValueError):
                 get_accelerometer_info(channel)
 
-def test_get_accelerometer_bounds(test_IDE):
+def test_get_accelerometer_bounds(test_IDE, test_PE_IDE, test_PR_IDE):
     """
     Tests that the subchannels g rating matches the parents value,
     that all g values are symmetric, and it's one of the g-ratings we 
     currently offer.
     """
-    acc_sub_chs = get_channels(test_IDE, "accel", True)
-    acc_chs = get_channels(test_IDE, "accel", False)
-    parent_gs = list(map(get_accelerometer_bounds, acc_chs))
-    for g in parent_gs:
-        assert -1 * g[0] == g[1]
-        assert g[1] in [8, 16, 25, 40, 100, 200, 500, 2000, 6000]
-    for ch in acc_sub_chs:
-        expected_g = parent_gs[acc_chs.index(ch.parent)]
-        assert get_accelerometer_bounds(ch) == expected_g
+    for ide in [test_IDE, test_PE_IDE, test_PR_IDE]:
+        acc_sub_chs = get_channels(ide, "accel", True)
+        acc_chs = get_channels(ide, "accel", False)
+        parent_gs = list(map(get_accelerometer_bounds, acc_chs))
+        for g in parent_gs:
+            assert -1 * g[0] == g[1]
+            assert g[1] in [8, 16, 25, 40, 100, 200, 500, 2000, 6000]
+        for ch in acc_sub_chs:
+            expected_g = parent_gs[acc_chs.index(ch.parent)]
+            assert get_accelerometer_bounds(ch) == expected_g
